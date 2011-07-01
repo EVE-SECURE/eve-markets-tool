c7aad312fd9f455650ce994df5cc5d4769185d9e
div@yandex.ru <div@yandex.ru@afc7a6f1-3c5b-0ddf-2d09-5798fe8441ae>
2011-07-01 01:33:54 +0000

    svn:ignore part

---
+++ trunk/client/scripts/run.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set _ROOT=%~dp0
+set _JAVACMD=%JAVACMD%
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java.exe
+goto runApp
+
+:noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=java.exe
+
+:runApp
+%_JAVACMD% -cp %_ROOT%;evemarketstool.jar org.shadanakar.eve.markets.Main
+
+ 
